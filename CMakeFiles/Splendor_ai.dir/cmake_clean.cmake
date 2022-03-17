490658f9e5d51fadd707f3dee18fe0a375ce6c10
drabart <drabart@outlook.com>
2022-03-17 15:59:04 +0100

    Fixed double move bug

---
+++ CMakeFiles/Splendor_ai.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Splendor_ai.dir/interface.cpp.o"
+  "Splendor_ai"
+  "Splendor_ai.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Splendor_ai.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
